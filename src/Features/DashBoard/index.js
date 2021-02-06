ae93c744ae8ac284674bc14893ab0ca6ab89b49b
Tran Trung Duc <ductt@awing.vn>
2021-02-06 23:44:58 +0700

    first create react native app with Dashboard

---
+++ src/Features/DashBoard/index.js
@@ -0,0 +1,11 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { Text, View } from 'react-native';
+
+export default function Dashboard() {
+  return (
+    <View>
+      <Text>I'm Dashboard</Text>
+    </View>
+  );
+}
---
+++ src/Ultilities/Theme.js
@@ -0,0 +1,9 @@
+const THEMES = {
+    CLASSIC: {
+        PRIMARY_COLOR: '#ED1D25',
+        SECONDARY_COLOR: '#3B86FF',
+        
+    }
+}
+
+export default THEMES;
