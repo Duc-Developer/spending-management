e57bafb8b4465bd982223a50cd9d34b03c115260
Tran Trung Duc <ductt@awing.vn>
2021-02-15 15:05:22 +0700

    canh bao nguoi dung khi nhap sai mat khau hoac email

--- src/Features/Auth/Login/component.js
+++ src/Features/Auth/Login/component.js
@@ -3,9 +3,10 @@ import { Container, Form, Item, Input, Label, Button, Text, Icon } from 'native-
 import styles from './Style';
 import { View, Image } from 'react-native';
 import Logo from '../../../assets/paypal-letter-logo-in-a-circle.png';
+import { ERRORS_CODE } from '../../../Ultilities/EnumAuth';
 
 export default function LoginComponent(props) {
-    const { handleSubmit } = props;
+    const { handleSubmit, errors, setErrors } = props;
     const [secureTextEntry, setSecureTextEntry] = useState(true);
     const [user, setUser] = useState({
         email: '',
@@ -17,6 +18,12 @@ export default function LoginComponent(props) {
             ...user,
             [fieldName]: text
         });
+        if (errors.code) {
+            setErrors({
+                code: null,
+                message: null,
+            });
+        }
     }
 
     function switchTextEntry() {
@@ -36,6 +43,14 @@ export default function LoginComponent(props) {
                             value={user.email}
                             onChangeText={(text) => { handleChangeText("email", text) }}
                         />
+                        {
+                            (errors.code === ERRORS_CODE.INVALID_EMAIL || errors.code === ERRORS_CODE.USER_NOT_FOUND)
+                                ? <Icon
+                                    type="FontAwesome"
+                                    name="exclamation"
+                                />
+                                : <></>
+                        }
                     </Item>
                     <Item floatingLabel >
                         <Label style={styles.inputLabel}>Password</Label>
@@ -44,6 +59,14 @@ export default function LoginComponent(props) {
                             value={user.password}
                             onChangeText={(text) => { handleChangeText("password", text) }}
                         />
+                        {
+                            errors.code === ERRORS_CODE.WRONG_PASSWORD
+                                ? <Icon
+                                    type="FontAwesome"
+                                    name="exclamation"
+                                />
+                                : <></>
+                        }
                         {
                             secureTextEntry
                                 ? <Icon
--- src/Features/Auth/Login/container.js
+++ src/Features/Auth/Login/container.js
@@ -1,29 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 import firebaseApp from '../../../../firebase';
 import LoginComponent from './component';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export default function LoginContainer() {
-
-    const handleSubmit = ({ email, password }) => {
-        console.log(email, password)
-        firebaseApp.auth().signInWithEmailAndPassword(email, password)
-  .then((userCredential) => {
-    // Signed in
-    var user = userCredential.user;
-    console.log(user)
-    // ...
-  })
-  .catch((error) => {
-    var errorCode = error.code;
-    var errorMessage = error.message;
+  const [errors, setErrors] = useState({
+    code: null,
+    message: null,
   });
-    }
 
-    return (
-        <React.Fragment>
-            <LoginComponent
-                handleSubmit={handleSubmit}
-            />
-        </React.Fragment>
-    );
+  const handleSubmit = ({ email, password }) => {
+    async function loginWithEmailAndPassword() {
+      const user = await firebaseApp.auth().signInWithEmailAndPassword(email, password)
+        .then((userCredential) => {
+          // Signed in
+          return userCredential.user;
+          // ...
+        })
+        .catch((error) => {
+          const errorMessage = error.message;
+          const errorCode = error.code;
+          setErrors({
+            code: errorCode,
+            message: errorMessage,
+          });
+          alert(errorMessage);
+        });
+      if (!user) {
+        return;
+      } else {
+        await AsyncStorage.setItem('uid', user.uid);
+      }
+    }
+    loginWithEmailAndPassword();
+  }
+
+  return (
+    <React.Fragment>
+      <LoginComponent
+        handleSubmit={handleSubmit}
+        errors={errors}
+        setErrors={setErrors}
+      />
+    </React.Fragment>
+  );
 }
